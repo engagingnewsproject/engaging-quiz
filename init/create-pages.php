c3ac9f245a08ee7b8b3990451d0e3bd201eeb610
Jerry <jones.jeremydavid@gmail.com>
2015-10-19 10:27:33 -0500

    removed commented out template arguments from create pages, and removed assign to author. hopefully it will assign to current user otherwise

--- init/create-pages.php
+++ init/create-pages.php
@@ -5,63 +5,47 @@
 *
 */
 $configure_quiz = array(
-  // 'page_template' => 'self-service-quiz/page-configure-quiz.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'Configure Quiz',
   'post_content'  => '[configure_quiz]',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 $quiz_report = array(
-  // 'page_template' => 'self-service-quiz/page-quiz-view.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'Quiz Report',
   'post_content'  => '[quiz_report]',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 $quiz_answer = array(
   'page_template' => 'self-service-quiz/page-quiz-answer.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'Quiz Answer',
   'post_content'  => '',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 $iframe_quiz = array(
   'page_template' => 'self-service-quiz/page-iframe-quiz.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'iframe quiz',
   'post_content'  => '',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 $create_a_quiz = array(
-  // 'page_template' => 'self-service-quiz/page-create-a-quiz.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'Create a Quizz',
   'post_content'  => '[create-a-quiz]',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 $view_quiz = array(
-  // 'page_template' => 'self-service-quiz/page-quiz-view.php',
-  // 'page_template' => 'template-full-width.php',
   'post_type'     => 'page',
   'post_title'    => 'View Quiz',
   'post_content'  => '[view_quiz]',
-  'post_status'   => 'publish',
-  'post_author'   => 4
+  'post_status'   => 'publish'
 );
 
 if( !get_page_by_title('Configure Quiz') ) {
@@ -72,4 +56,5 @@ if( !get_page_by_title('Configure Quiz') ) {
   wp_insert_post( $create_a_quiz );
   wp_insert_post( $view_quiz );
 }
+
 ?>
