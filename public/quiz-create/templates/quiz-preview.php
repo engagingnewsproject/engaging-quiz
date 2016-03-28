725b9f400a85bda90876286cd8e279e871cf9a23
Jerry <jones.jeremydavid@gmail.com>
2016-03-28 14:06:29 -0500

    switched bg color and text color to text inputs. added width input.

--- public/quiz-create/templates/quiz-preview.php
+++ public/quiz-create/templates/quiz-preview.php
@@ -21,31 +21,36 @@
                 <input type="hidden" name="enp_quiz[quiz_id]" value="<? echo $quiz->get_quiz_id();?>" />
                 <fieldset class="enp-fieldset enp-title-display">
                     <legend class="enp-legend enp-title-display__legend">Title Display</legend>
-                    <input id="enp-quiz-title-show" class="enp-radio enp-title-display__input enp-title-display__input--title-display" type="radio" name="enp_quiz[title-display]" value="title-show" checked="checked"/>
+                    <input id="enp-quiz-title-show" class="enp-radio enp-title-display__input enp-title-display__input--title-display" type="radio" name="enp_quiz[quiz_title_display]" value="title-show" checked="checked"/>
                     <label class="enp-label enp-title-display__label enp-title-display__label--title-display" for="enp-quiz-title-show">
                         Show Title
                     </label>
-                    <input class="enp-radio enp-title-display__input enp-title-display__input--hide-title" id="enp-quiz-title-hide" type="radio" name="enp_quiz[title-display]" value="title-hide"/>
+                    <input class="enp-radio enp-title-display__input enp-title-display__input--hide-title" id="enp-quiz-title-hide" type="radio" name="enp_quiz[quiz_title_display]" value="title-hide"/>
                     <label class="enp-label enp-title-display__label enp-title-display__label--hide-title" for="enp-quiz-title-hide">
                         Hide Title
                     </label>
                 </fieldset>
 
-                <fieldset class="enp-fieldset enp-quiz-bg-color">
-                    <legend class="enp-legend enp-quiz-bg-color__legend">Background Color</legend>
-                    <input id="white" class="enp-radio enp-quiz-bg-color__input enp-quiz-bg-color__input--white" type="radio" name="enp_quiz[enp-quiz-bg-color]" value="#ffffff" checked="checked"/>
-                    <label class="enp-label enp-quiz-bg-color__label enp-quiz-bg-color__label--white" for="white">
-                        White
+                <fieldset class="enp-fieldset enp-quiz-styles">
+                    <legend class="enp-legend enp-quiz-styles__legend enp-screen-reader-text">Quiz Styles</legend>
+
+                    <label class="enp-label enp-quiz-styles__label enp-quiz-styles__label--width" for="enp-quiz-width">
+                        Width
                     </label>
+                    <input id="enp-quiz-width" class="enp-input enp-quiz-styles__input enp-quiz-styles__input--width" type="text" name="enp_quiz[quiz_width]" value="100%"/>
+
+                    <label class="enp-label enp-quiz-styles__label enp-quiz-styles__label--bg-color" for="enp-quiz-bg-color">
+                        Background Color
+                    </label>
+                    <input id="enp-quiz-bg-color" class="enp-input enp-quiz-styles__input enp-quiz-styles__input--bg-color" type="text" name="enp_quiz[quiz_bg-color]" value="#ffffff"/>
+
+                    <label class="enp-label enp-quiz-styles__label enp-quiz-styles__label--text-color" for="enp-quiz-text-color">
+                        Text Color
+                    </label>
+                    <input id="enp-quiz-text-color" class="enp-input enp-quiz-styles__input enp-quiz-styles__input--text-color" type="text" name="enp_quiz[quiz_text-color]" value="#333333"/>
+
                 </fieldset>
 
-                <fieldset class="enp-fieldset enp-quiz-text-color">
-                    <legend class="enp-legend enp-quiz-text-color__legend">Text Color</legend>
-                    <input id="black" class="enp-radio enp-quiz-text-color__input enp-quiz-text-color__input--black" type="radio" name="enp_quiz[enp-quiz-text-color]" value="title" checked="checked"/>
-                    <label class="enp-label enp-quiz-text-color__label enp-quiz-text-color__label--black" for="black">
-                        Black
-                    </label>
-                </fieldset>
 
                 <button type="submit" class="enp-btn--submit enp-btn--next-step enp-preview-form__submit" name="enp-quiz-submit" value="quiz-publish">Publish <svg class="enp-icon enp-icon--chevron-right enp-btn--next-step__icon enp-preview-form__submit__icon">
                   <use xlink:href="#icon-chevron-right" />
