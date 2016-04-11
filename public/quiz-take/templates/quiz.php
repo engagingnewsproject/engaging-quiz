95e8cb7657238a60e6c5004dcbba7d558402a7e0
Jerry <jones.jeremydavid@gmail.com>
2016-04-11 16:09:20 -0500

    start of parsing resources for quiz take

--- public/quiz-take/templates/quiz.php
+++ public/quiz-take/templates/quiz.php
@@ -1,81 +1,5 @@
 <html lang="en-US">
 <head>
-    <style>
-    html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, font, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td {
-    border: 0;
-    font-family: inherit;
-    font-size: 100%;
-    font-style: inherit;
-    font-weight: inherit;
-    margin: 0;
-    outline: 0;
-    padding: 0;
-    vertical-align: baseline;
-}
-html {
-    font-size: 100%; /* Corrects text resizing oddly in IE6/7 when body font-size is set using em units http://clagnut.com/blog/348/#c790 */
-    overflow-y: scroll; /* Keeps page centred in all browsers regardless of content height */
-    -webkit-text-size-adjust: 100%; /* Prevents iOS text size adjust after orientation change, without disabling user zoom */
-    -ms-text-size-adjust: 100%; /* www.456bereastreet.com/archive/201012/controlling_text_size_in_safari_for_ios_without_disabling_user_zoom/ */
-}
-body {
-    overflow: hidden;
-}
-article,
-aside,
-details,
-figcaption,
-figure,
-footer,
-header,
-nav,
-section {
-    display: block;
-}
-ol, ul {
-    list-style: none;
-}
-table { /* tables still need 'cellspacing="0"' in the markup */
-    border-collapse: separate;
-    border-spacing: 0;
-}
-caption, th, td {
-    font-weight: normal;
-    text-align: left;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-    content: "";
-}
-blockquote, q {
-    quotes: "" "";
-}
-a:focus {
-    outline: thin dotted;
-}
-a:hover,
-a:active { /* Improves readability when focused and also mouse hovered in all browsers people.opera.com/patrickl/experiments/keyboard/test */
-    outline: 0;
-}
-a img {
-    border: 0;
-}
-
-/* Reseting to border-box so we can do 100% width with 20px and not mess stuff up. */
-html {
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}</style>
     <? if(isset($_GET['quiz_id'])) {
         $quiz_id = $_GET['quiz_id'];
         // set enp-quiz-config file path
@@ -87,6 +11,8 @@ html {
         require ENP_QUIZ_PLUGIN_DIR . 'includes/class-enp_quiz-mc_option.php';
         // Database
         require ENP_QUIZ_PLUGIN_DIR . 'database/class-enp_quiz_db.php';
+
+
         // set-up our quiz
         $quiz = new Enp_quiz_Quiz($quiz_id);
         if($quiz->get_quiz_id() === null) {
@@ -97,9 +23,13 @@ html {
         echo 'No Quiz Requested';
         exit;
     }?>
-    <link rel="stylesheet" type="text/css" href="<? echo ENP_QUIZ_PLUGIN_URL;?>public/quiz-create/css/enp_quiz-create.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="<? echo ENP_QUIZ_PLUGIN_URL;?>public/quiz-take/css/enp_quiz-take.min.css" media="all" />
 </head>
 <body id="enp-quiz">
+<?php //add in our SVG
+    $svg = file_get_contents(ENP_QUIZ_PLUGIN_URL.'/public/quiz-take/svg/symbol-defs.svg');
+    echo $svg;
+?>
 <section class="enp-quiz__container">
     <style tyle="text/css">
         #enp-quiz .enp-quiz__container {
