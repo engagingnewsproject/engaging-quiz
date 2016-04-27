8840e17e089581aa786439f6f5475c063baa0070
Jerry <jones.jeremydavid@gmail.com>
2016-04-27 09:47:19 -0500

    full width bg when answering questions. margin bottom on question image

--- public/quiz-take/templates/partials/question-image.php
+++ public/quiz-take/templates/partials/question-image.php
@@ -1,5 +1,5 @@
 <img
-    class="enp-question-image enp-question-image"
+    class="enp-question-image"
     src="<?php echo $qt_question->question->get_question_image_src();?>"
     srcset="<?php echo $qt_question->question->get_question_image_srcset();?>"
     alt="<?php echo $qt_question->question->get_question_image_alt();?>"
