14b5a1c77dd2aecea8b0d0a52ddd56c1605e3d46
Jerry <jones.jeremydavid@gmail.com>
2016-05-09 15:08:32 -0500

    start of ab test question responses

---
+++ includes/class-enp_quiz-question_ab_test_result.php
@@ -0,0 +1,71 @@
+<?
+/**
+* Overrides data for the question object and sets it from the AB Test results instead
+* Create a question object
+* @param $question_id = the id of the question you want to get
+* @return question object
+*/
+class Enp_question_Question_AB_test_result extends Enp_question_Question {
+    public static $results;
+
+    public function __construct($question_id, $ab_test_id = false) {
+        parent::__construct($question_id);
+
+        if($ab_test_id === false) {
+            return false;
+        }
+
+        $this->get_ab_test_question_results($question_id, $ab_test_id);
+    }
+
+    public function get_ab_test_question_results($question_id, $ab_test_id) {
+        self::$results = $this->select_ab_test_question_results($question_id, $ab_test_id);
+        if(self::$results !== false) {
+            self::$results = $this->set_ab_test_question_results();
+        }
+        return self::$results;
+    }
+
+    public function select_ab_test_question_results($question_id, $ab_test_id) {
+        $pdo = new enp_question_Db();
+        // Do a select query to see if we get a returned row
+        $params = array(
+            ":ab_test_id" => $ab_test_id,
+            ":question_id" => $question_id
+        );
+        $sql = "SELECT * from ".$pdo->response_ab_test_table." ab_response
+            INNER JOIN ".$pdo->response_question_table." question_response
+                    ON ab_response.response_question_id = question_response.response_quiz_id
+                 WHERE ab_response.ab_test_id = :ab_test_id
+                   AND question_response.question_id = :question_id ";
+        $stmt = $pdo->query($sql, $params);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // return the found results
+        return $results;
+    }
+
+    public function set_ab_test_question_results() {
+        $this->question_views = $this->set_question_views();
+        $this->question_starts = $this->set_question_responses();
+    }
+
+    public function set_question_views() {
+        return count(self::$results);
+    }
+
+    public function set_question_responses() {
+        $responses = 0;
+        if(!empty(self::$results)) {
+            foreach(self::$results as $result) {
+                if($result['question_responded'] === '1') {
+                    $responses++;
+                }
+            }
+        }
+        return $responses;
+    }
+
+
+
+}
