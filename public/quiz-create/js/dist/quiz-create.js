cb5817e1eaae9bde5bbf09faa0d7f149cd27ffbf
Jerry <jones.jeremydavid@gmail.com>
2016-05-17 15:59:27 -0500

    added icon to accordion headers

--- public/quiz-create/js/dist/quiz-create.js
+++ public/quiz-create/js/dist/quiz-create.js
@@ -246,7 +246,7 @@ $(document).on('keyup', '.enp-question-title__textarea', function() {
     // get the value of the textarea we're typing in
     question_title = $(this).val();
     // find the accordion header it goes with and add in the title
-    $(this).closest('.enp-question-content').prev('.enp-accordion-header').text(question_title);
+    $(this).closest('.enp-question-content').prev('.enp-accordion-header').find('.enp-accordion-header__title').text(question_title);
 });
 
 // append ajax response message
