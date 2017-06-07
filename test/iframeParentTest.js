b4f3c19bb1767d8b9dcd2555130f0a4db9e2ecbb
Jerry <jones.jeremydavid@gmail.com>
2017-06-06 20:44:21 -0500

    setup testing with mocha

---
+++ test/iframeParentTest.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var iframeParent = require('../public/quiz-take/dist/iframe-parent.min.js');
+
+describe('iframeParent', function() {
+
+    before(function() {
+        // runs before all tests in this block
+    });
+
+    after(function() {
+        // runs after all tests in this block
+    });
+
+    beforeEach(function() {
+        // runs before each test in this block
+    });
+
+    afterEach(function() {
+        // runs after each test in this block
+    });
+
+    // test cases
+    describe('saveEnpEmbedSite', function() {
+        it('should return json for new site when saved correctly', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
