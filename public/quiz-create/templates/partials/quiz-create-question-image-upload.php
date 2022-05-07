b280a06eeca6cf4334ebc5673aa0733f123b4f80
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-05-07 11:26:21 -0700

    NAME attribute on the question image upload element prevents save if commented out

--- public/quiz-create/templates/partials/quiz-create-question-image-upload.php
+++ public/quiz-create/templates/partials/quiz-create-question-image-upload.php
@@ -1,6 +1,6 @@
 <!-- question image upload -->
 <label for="enp-question-image-upload-<?php echo $question_id;?>" class="enp-label enp-image-upload__label">Add Image</label>
-
+<!-- EUREKA!!!!: NAME attribute on the question image upload element prevents save if commented out -->
 <input id="enp-question-image-upload-<?php echo $question_id;?>" type="file" accept="image/*" class="enp-question-image-upload__input" name="question_image_upload_<?php echo $question_id;?>">
 
 <button class="enp-button enp-quiz-submit enp-button__question-image-upload" name="enp-quiz-submit" value="question-image--upload-<?php echo $question_id;?>">Upload Image</button>
